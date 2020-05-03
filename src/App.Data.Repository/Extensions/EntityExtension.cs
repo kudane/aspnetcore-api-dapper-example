2086a1392cd1a0088cd8585ba4ea10adabf49260
kudane <kraisorn204@gmail.com>
2020-05-03 20:47:09 +0700

    สร้าง EntityExtension.cs เช็คค่า null

---
+++ src/App.Data.Repository/Extensions/EntityExtension.cs
@@ -0,0 +1,20 @@
+﻿using App.Data.Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Data.Repository.Extensions
+{
+    public static class EntityExtension
+    {
+        public static bool IsNotNull(this Entity entity)
+        {
+            return entity != null;
+        }
+
+        public static bool IsNull(this Entity entity)
+        {
+            return entity is null;
+        }
+    }
+}
