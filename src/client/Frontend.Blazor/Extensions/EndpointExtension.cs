7384b072317cdfef8c3074535a621c5a5c55fe0f
kudane <kraisorn204@gmail.com>
2020-05-09 17:38:35 +0700

    frontend blazor -แสดงเลขหน้าและสามารถกดเปลี่ยนได้ -จัดการ event ให้สามารถคิวรี่ข้อมูลใหม่ -เขียนฟังก์ชั่นคำนวณเลขหน้า

---
+++ src/client/Frontend.Blazor/Extensions/EndpointExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Frontend.Blazor.Extensions
+{
+    public class EndpointExtension
+    {
+        public static string GetMovie(int genreId, int pageSize, int pageNumber)
+        {
+            return $"api/genre/{genreId}/movies?pagesize={pageSize}&pagenumber={pageNumber}";
+        }
+    }
+}
---
+++ src/client/Frontend.Blazor/ViewModels/PaginationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Frontend.Blazor.ViewModels
+{
+    public class PaginationViewModel
+    {
+        public class Pager
+        {
+            public string Text { get; set; } = "0";
+            public bool IsActive { get; set; } = false;
+        }
+
+        public List<Pager> PageItems { get; set; } = new List<Pager>(0);
+    }
+}
